e64eec255a03247185508f10a9be7174cb44c2c3
reddyprasanth3 <reddyprasanth2222@gmail.com>
2026-06-16 12:05:16 +0530

    New Branch j

--- dict.py
+++ dict.py
@@ -1,5 +1,10 @@
 # dict = {"company":"tata", "model":"suv", "year":1996}
 # print(dict)
 
-dict = {"company":"tata", "model":"suv", "year":1996, "color":["white", "blue","yellow"]}
-print(dict)
+# dict = {"company":"tata", "model":"suv", "year":1996, "color":["white", "blue","yellow"]}
+# print(dict)
+
+
+dict = {"company":"tata", "model":"suv", "year":1996}
+print(dict["model"])
+print('sai')
